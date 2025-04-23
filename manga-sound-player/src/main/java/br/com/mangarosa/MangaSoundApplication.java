fab1264005b319ddeb2eee9dc8647e013b4eabe2
Pedro Tigre <pedrotigregss@gmail.com>
2025-04-23 19:40:22 -0300

    Correção do MangaSoundApplication

--- manga-sound-player/src/main/java/br/com/mangarosa/MangaSoundApplication.java
+++ manga-sound-player/src/main/java/br/com/mangarosa/MangaSoundApplication.java
@@ -1,9 +1,22 @@
 package br.com.mangarosa;
 
-public class MangaSoundApplication
-{
-    public static void main( String[] args ) {
+import br.com.mangarosa.collections.*;
 
-        System.out.println( "Welcome to Manga Sound Player" );
+public class MangaSoundApplication {
+    public static void main(String[] args) {
+        System.out.println("🎵 Welcome to Manga Sound Player 🎵");
+
+        Playlist playlist = new Playlist();
+
+        playlist.adicionarMusica(new Music("", ""));
+
+
+        System.out.println("▶ Playlist original:");
+        playlist.listarMusicas();
+
+        playlist.moverMusica(2, 0);
+
+        System.out.println("\n🔀 Playlist após mover música da posição 2 para a 0:");
+        playlist.listarMusicas();
     }
 }
