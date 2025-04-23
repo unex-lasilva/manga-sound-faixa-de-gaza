366749697858fc6f0615501395de465cb2c91020
Pedro Tigre <pedrotigregss@gmail.com>
2025-04-23 19:32:25 -0300

    Adiciona método para listar músicas na Playlist
    Implementa o método listarMusicas na classe Playlist, que permite
    exibir todas as músicas presentes na playlist, utilizando a lista
    duplamente encadeada.

---
+++ manga-sound-player/src/main/java/br/com/mangarosa/collections/Playlist.java
@@ -0,0 +1,74 @@
+package br.com.mangarosa.collections;
+
+public class Playlist {
+    private PlaylistNode head;
+    private PlaylistNode tail;
+
+    public void adicionarMusica(Music music) {
+        PlaylistNode novo = new PlaylistNode(music);
+        if (head == null) {
+            head = tail = novo;
+        } else {
+            tail.setNext(novo);
+            novo.setPrev(tail);
+            tail = novo;
+        }
+    }
+
+    public void listarMusicas() {
+        PlaylistNode atual = head;
+        while (atual != null) {
+            System.out.println(atual.getMusic());
+            atual = atual.getNext();
+        }
+    }
+
+    public void moverMusica(int de, int para) {
+        if (de == para || head == null) return;
+
+        PlaylistNode atual = head;
+        int pos = 0;
+
+        // Encontrar o nó a ser movido
+        while (atual != null && pos < de) {
+            atual = atual.getNext();
+            pos++;
+        }
+
+        if (atual == null) return;
+
+        // Remover o nó
+        PlaylistNode anterior = atual.getPrev();
+        PlaylistNode proximo = atual.getNext();
+
+        if (anterior != null) anterior.setNext(proximo);
+        else head = proximo;
+
+        if (proximo != null) proximo.setPrev(anterior);
+        else tail = anterior;
+
+        // Reposicionar
+        PlaylistNode destino = head;
+        int i = 0;
+        while (destino != null && i < para) {
+            destino = destino.getNext();
+            i++;
+        }
+
+        if (destino == null) {
+            // Insere no final
+            tail.setNext(atual);
+            atual.setPrev(tail);
+            atual.setNext(null);
+            tail = atual;
+        } else {
+            PlaylistNode destinoAnterior = destino.getPrev();
+            atual.setNext(destino);
+            atual.setPrev(destinoAnterior);
+
+            destino.setPrev(atual);
+            if (destinoAnterior != null) destinoAnterior.setNext(atual);
+            else head = atual;
+        }
+    }
+}
---
+++ manga-sound-player/src/main/java/br/com/mangarosa/collections/PlaylistNode.java
@@ -0,0 +1,31 @@
+package br.com.mangarosa.collections;
+
+public class PlaylistNode {
+    private Music music;
+    private PlaylistNode next;
+    private PlaylistNode prev;
+
+    public PlaylistNode(Music music) {
+        this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public PlaylistNode getNext() {
+        return next;
+    }
+
+    public void setNext(PlaylistNode next) {
+        this.next = next;
+    }
+
+    public PlaylistNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(PlaylistNode prev) {
+        this.prev = prev;
+    }
+}
